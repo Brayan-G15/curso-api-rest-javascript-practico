257f4e725faac3592a4cf0ecd91f0a7c8b1865b3
Brayan-g15 <b.sleider15@gmail.com>
2022-08-04 10:58:56 -0500

    funcion de lista de categorias

--- src/main.js
+++ src/main.js
@@ -19,9 +19,37 @@ async function getTrendingMoviesPreview() {
         //Metemos las etiquetas dentro de otras etiquetas para dar la estructura qu trae el HTML
         movieContainer.appendChild(movieImg);
         trendingPreviewMoviesContainer.appendChild(movieContainer);
-    });
+
 
     console.log(movies);
+    });
+}
+
+
+async function getGenresMoviesPreview() {
+    const res = await fetch(`https://api.themoviedb.org/3/genre/movie/list?api_key=${API_KEY}`);
+    const data = await res.json();
+
+    const categories = data.genres; // Arroja un array
+    //Manipulacion del DOM
+    categories.forEach(category => {
+        const previewCategoriesContainer = document.querySelector("#categoriesPreview .categoriesPreview-list");//Guarda la etiqueta del HTML en una constante
+
+        const categoryContainer = document.createElement("div"); //Crea una nueva etiqueta div en el HTML 
+        categoryContainer.classList.add("category-container"); // Mete la eqitueta creada en dentro de la etiqueta que se pasa en el id parentesis
+
+        const categoryTitle = document.createElement("h3"); //Crea una nueva etiqueta h3 en el HTML
+        categoryTitle.classList.add("category-title"); // Mete la etiqueta creada dentro de la etiqueta que se pasa en el id parentesis
+        categoryTitle.setAttribute("id", category.id); //Le pasamos el id que nos arroja el array de genres
+        const categoryTitleText = document.createTextNode(category.name); //Ponemos el texto de la categoria
+
+
+        //Metemos las etiquetas dentro de otras etiquetas para dar la estructura qu trae el HTML
+        categoryTitle.appendChild(categoryTitleText);
+        categoryContainer.appendChild(categoryTitle);
+        previewCategoriesContainer.appendChild(categoryContainer);
+    });
 }
 
 getTrendingMoviesPreview();
+getGenresMoviesPreview();
